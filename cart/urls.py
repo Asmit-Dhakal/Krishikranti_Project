9c6b310162f1615e8d7e024c7620408f5592058e
Asmit Dhakal <107795383+Asmit-Dhakal@users.noreply.github.com>
2024-08-21 15:10:18 +0545

    commit

--- cart/urls.py
+++ cart/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('cart/',CartView.as_view(),name='cart'),
-
+    path('orders/', OrderAPIView.as_view(), name='orders'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
